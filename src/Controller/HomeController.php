337bda5dcf7735470ab17796f75e384dcdd7c430
Benoit PETIT <petitbenoit3@gmail.com>
2022-02-21 21:30:15 +0100

    commit

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ProductRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('/', name: 'home')]
+    public function index(ProductRepository $productRepository): Response
+    {
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+            'products' => $productRepository->findAll()
+        ]);
+    }
+}
